06e0ce6ca405aa2b83fdd27677a43388a48f7d14
c-matsty <matthaios.stylianidis@assaabloy.com>
2020-08-13 22:39:27 +0200

    Reconstruction error evaluation script

---
+++ src/feature_evaluation/reconstruction_test.py
@@ -0,0 +1,140 @@
+"""
+Evaluates the encoder of the Bi-GAN by calculating the reconstruction loss for the latent variable, generating
+a number of latent samples, feeding them through the generator and reconstructing them back through the encoder.
+"""
+
+__author__ = "Matthaios Stylianidis"
+
+import os
+import re
+
+import numpy as np
+import argparse
+import matplotlib.pyplot as plt
+from tqdm import tqdm
+
+import gantools.utils as utils
+from feature_evaluation.utils import get_avg_reconstruction_error
+
+os.environ["CUDA_VISIBLE_DEVICES"] = "3"
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
+
+DEFAULT_NAME = "wgan"
+DEFAULT_SAVE_DIR = os.path.join("..", "results")
+DEFAULT_LATENT_DIM = 100
+DEFAULT_EPSILON = 0.0
+
+
+def get_arguments():
+    """Parse all the arguments provided from the CLI.
+
+    Returns:
+      A list of parsed arguments.
+    """
+    parser = argparse.ArgumentParser(description="Evaluate reconstruction error over time.")
+    parser.add_argument("--dataset-size", type=int, required=False, default=1000,
+                        help="Number of latent variables to sample.")
+    parser.add_argument("--checkpoint-dir", type=str, required=True,
+                        help="Path to the directory with all the checkpoints for the model.")
+    parser.add_argument("--save-dir", type=str, required=False, default=DEFAULT_SAVE_DIR,
+                        help="Path to the directory where results should be saved.")
+    parser.add_argument("--model-name", type=str, required=False, default=DEFAULT_NAME,
+                        help="The name of the trained model included in the checkpoint file names.")
+    parser.add_argument("--latent-dim", type=int, required=False, default=DEFAULT_LATENT_DIM,
+                        help="Dimensionality of the latent variable.")
+    parser.add_argument("--epsilon", type=int, required=False, default=DEFAULT_EPSILON,
+                        help="Epsilon to be used for a smoothness test.")
+    return parser.parse_args()
+
+
+def get_checkpoint_paths(checkpoints_dir, name):
+    """ Gets a list of tuples with the update step values along with their corresponding checkpoint paths.
+
+    Args:
+        checkpoints_dir: The directory with all the checkpoints to be sequentially evaluated.
+        name: The name of the trained model used in the checkpoint filenames.
+
+    Returns:
+        A list with tuples where the first element is an integer denoting the update step and the second is a string
+            with the path to checkpoint for that update step
+    """
+    filenames = os.listdir(checkpoints_dir)
+    # Remove extension from file names
+    filenames = [filename.split('.')[0] for filename in filenames]
+    # Remove duplicate names resulting from multiple files needed for the same checkpoint
+    filenames = np.unique(filenames)
+
+    # Get update step from each filename and create tuple list
+    res = []
+    update_step_pattern = "{}-[0-9]+".format(name)
+    for filename in tqdm(filenames):
+        try:
+            step_string = re.findall(update_step_pattern, filename)[0]
+            update_step = int(step_string.split('-')[-1])
+            res.append((update_step, os.path.join(checkpoints_dir, filename)))
+        except IndexError:
+            # Exception will be caught for files that do not abide by the checkpoint format - files that we do not need
+            continue
+
+    # Sort tuple list according to update step by ascending order
+    res.sort(key=lambda tup: tup[0])
+
+    return res
+
+
+if __name__ == "__main__":
+    args = get_arguments()
+    dataset_size = args.dataset_size
+    checkpoint_dir = args.checkpoint_dir
+    save_dir = args.save_dir
+    name = args.model_name
+    latent_dim = args.latent_dim
+    epsilon = args.epsilon
+
+    print("-Getting and sorting checkpoint paths according to update step")
+    checkpoint_tuples = get_checkpoint_paths(checkpoint_dir, name=name)
+
+    print("-Generating latent samples")
+    Z = utils.sample_latent(dataset_size, latent_dim)
+
+    print("-Start reconstructing latent samples for each checkpoint")
+    avg_reconstruction_error = get_avg_reconstruction_error(Z, checkpoint_tuples, epsilon)
+    values = np.array(list(avg_reconstruction_error.values()))
+
+    print("-Visualizing and saving result")
+    title_size = 16
+    label_size = 13
+    title = "Reconstruction error over time"
+    log_title = "Reconstruction error over time - Log scale"
+    if epsilon != 0.0:
+        title += "$\epsilon$ = {}".format(epsilon)
+        log_title += "$\epsilon$ = {}".format(epsilon)
+
+    if not os.path.exists(save_dir):
+        os.makedirs(save_dir)
+    plt.plot(list(avg_reconstruction_error.keys()), values)
+    plt.title(title, size=title_size)
+    plt.xlabel("Update step", size=label_size)
+    plt.ylabel("Reconstruction error", size=label_size)
+    plt.savefig(os.path.join(save_dir, 'reconstruction_error.png'))
+    plt.cla()
+
+    plt.plot(list(avg_reconstruction_error.keys()), np.log(values))
+    plt.title(log_title, size=title_size)
+    plt.xlabel("Update step", size=label_size)
+    plt.ylabel("Log reconstruction error", size=label_size)
+    plt.savefig(os.path.join(save_dir, 'log_reconstruction_error.png'))
+    plt.cla()
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/feature_evaluation/utils.py
@@ -0,0 +1,72 @@
+"""
+Utility function for the different evaluation scripts.
+"""
+
+__author__ = "Matthaios Stylianidis"
+
+import os
+import sys
+
+import tensorflow as tf
+import numpy as np
+from tqdm import tqdm
+
+from gantools.model import BiSpectrogramGAN
+from gantools.gansystem import GANsystem
+from hyperparams.tifgan_hyperparams import get_hyperparams
+
+
+def get_avg_reconstruction_error(Z, checkpoint_tuples, epsilon=0.0, batch_size=64):
+    """ Calculates the mean absolute reconstruction error for every checkpoint.
+
+    Args:
+        Z (np.array): The dataset_size x latent_dimensions numpy array matrix with the latent samples to be
+            fed through the generator and the encoder to calculate the reconstruction error.
+        checkpoint_tuples (list): A list of tuples where each tuple contains the update step and the path to
+            the directory with the checkpoints.
+        epsilon (float): A number to be added to the latent samples before feeding them to the generator. This
+            argument can be set to small non zero values to evaluate the smoothness of the function learned by
+            the generator and the encoder. Defaults to 0.0.
+        batch_size (float): The number of latent to feed each time in parallel to the generator and encoder.
+
+    Returns:
+        A dictionary where the key is the update step corresponding to a checkpoint and the value is the
+            mean absolute reconstruction error acquired with that checkpoint.
+    """
+    avg_reconstruction_error = {}
+    dataset_size = Z.shape[0]
+
+    for update_step, checkpoint_path in tqdm(checkpoint_tuples):
+        # Get the parent directory to the checkpoint directory
+        results_path = os.path.join(checkpoint_path, "..", "..")
+
+        with tf.device('/gpu:0'):
+            params = get_hyperparams(results_path, "commands_md64_8k")
+            # Block print
+            sys.stdout = open(os.devnull, 'w')
+            biwgan = GANsystem(BiSpectrogramGAN, params)
+
+            with tf.Session() as sess:
+                biwgan.load(sess=sess, checkpoint=update_step)
+                # Enable print
+                sys.stdout = sys.__stdout__
+
+                cum_rec_error = 0
+                for i in range(0, len(Z), batch_size):
+                    z_batch = Z[i:i + batch_size] + epsilon
+                    x_batch = sess.run(biwgan._net.X_fake, feed_dict={biwgan._net.z: z_batch})
+                    z_batch_hat = sess.run(biwgan._net.z_real, feed_dict={biwgan._net.X_real: x_batch})
+                    # Compute sum of absolute errors for all latent samples
+                    cum_rec_error += np.sum(np.abs(z_batch - z_batch_hat), axis=1).sum()
+
+                # Divide the sum of all absolute errors by the dataset size to get the mean error
+                avg_reconstruction_error[update_step] = cum_rec_error / dataset_size
+
+    return avg_reconstruction_error
+
+
+
+
+
+
+
